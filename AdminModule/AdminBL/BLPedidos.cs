343fd6882bce9f61d684b11c31743a9d7b4917bf
josuevargasalvarez <josue.vargas@ucrso.info>
2018-06-20 20:54:00 -0600

    usuarios fiinal

---
+++ AdminModule/AdminBL/BLPedidos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdminBL
+{
+   public class BLPedidos
+    {
+        public DateTime fecha { get; set; }
+        public String estado { get; set; }
+        public int codigo { get; set; }
+        public String email { get; set; }
+
+        public BLPedidos()
+        {
+
+        }
+
+    }
+}
